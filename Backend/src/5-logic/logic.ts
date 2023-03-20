b7024b4c372377859baa277183b7c087b78ea146
Raz-N-oren <razoren2394@gmail.com>
2023-03-21 01:48:14 +0200

    Generated get target audience & get gift by ta

--- Backend/src/5-logic/logic.ts
+++ Backend/src/5-logic/logic.ts
@@ -1,8 +1,27 @@
 import dal from "../2-utils/dal";
+import GiftModel from "../4-models/gift-model";
+import TargetAudienceModel from "../4-models/target-audience-model";
 
+async function getAllTargetAudience(): Promise<TargetAudienceModel[]> {
+    const sql = `SELECT * FROM targetAudience`;
+    const targetAudience = await dal.execute(sql);
+    return targetAudience;
+}
 
+async function getGiftsByTargetAudience(targetAudienceId: number): Promise<GiftModel[]> {
+
+    const sql = `
+    SELECT G.*,T.targetAudienceName
+     FROM gifts AS G JOIN targetAudience AS T
+     ON G.targetAudienceId = T.targetAudienceId
+     WHERE G.targetAudienceId = ${targetAudienceId}`;
+
+    const gifts = await dal.execute(sql);
+    return gifts;
+}
 
 
 export default {
-
+    getAllTargetAudience,
+    getGiftsByTargetAudience
 };
--- Backend/src/6-controllers/controller.ts
+++ Backend/src/6-controllers/controller.ts
@@ -3,10 +3,23 @@ import logic from "../5-logic/logic";
 
 const router = express.Router(); // Capital R
 
-// GET http://localhost:3001/api/_____
-router.get("/_____", async (request: Request, response: Response, next: NextFunction) => {
+// GET http://localhost:3001/api/target-audience
+router.get("/target-audience", async (request: Request, response: Response, next: NextFunction) => {
     try {
+        const targetAudience = await logic.getAllTargetAudience();
+        response.json(targetAudience);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
 
+// GET http://localhost:3001/api/gifts-per-target-audience/:targetAudienceId
+router.get("/gifts-per-target-audience/:targetAudienceId", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const targetAudienceId = +request.params.targetAudienceId;
+        const gifts = await logic.getGiftsByTargetAudience(targetAudienceId);
+        response.json(gifts);
     }
     catch (err: any) {
         next(err);
