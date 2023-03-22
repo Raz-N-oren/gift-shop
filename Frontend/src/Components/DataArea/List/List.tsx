3b352af525560966b787efa1ad42a353e02b88cd
Raz-N-oren <razoren2394@gmail.com>
2023-03-22 14:37:31 +0200

    Service & gift card has been created

--- Frontend/src/Components/DataArea/List/List.tsx
+++ Frontend/src/Components/DataArea/List/List.tsx
@@ -1,9 +1,41 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import GiftModel from "../../../Models/GiftModel";
+import TargetAudienceModel from "../../../Models/TargetAudienceModel";
+import giftsService from "../../../Services/GiftsService";
 import "./List.css";
 
 function List(): JSX.Element {
+
+    const [gifts, setGifts] = useState<GiftModel[]>([]);
+    const [targetAudience, setTargetAudience] = useState<TargetAudienceModel[]>([]);
+
+    useEffect(() => {
+        giftsService.getAllTargetAudience()
+            .then(targetAudience => setTargetAudience(targetAudience))
+            .catch(err => alert(err.message))
+    }, []);
+
+    // HTMLSelectElement -> element raising the event
+    async function showGifts(args: ChangeEvent<HTMLSelectElement>) {
+
+        const value = +args.target.value;
+        giftsService.getGiftsByTargetAudience(value)
+            .then(gifts => setGifts(gifts))
+            .catch(err => alert(err.message));
+    }
+
     return (
         <div className="List">
-			<h2>List...</h2>
+            <label>Select Target Audience:</label>
+            <select defaultValue="" onChange={showGifts}>
+                <option disabled value="">Select...</option>
+                {targetAudience.map(t =>
+                    <option key={t.targetAudienceId} value={t.targetAudienceId}>
+                        {t.targetAudienceName}
+                    </option>)}
+            </select>
+
+
         </div>
     );
 }
